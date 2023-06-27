8c8ffecd20536742d611d7aa888a28a03e0acf06
Pritam Dhara <pritam1813@gmail.com>
2023-06-27 21:48:15 +0530

    Changed State to Object type

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ class App extends React.Component {
   }
 
   render() {
-    const movies = this.props.store.getState();
+    const { list } = this.props.store.getState();
     return (
       <div>
         <Navbar />
@@ -35,7 +35,7 @@ class App extends React.Component {
           </div>
 
           <div className="list">
-            {movies.map((movie, index) => (
+            {list.map((movie, index) => (
               <MovieCard movie={movie} key={`movie-${index}`} />
             ))}
           </div>
