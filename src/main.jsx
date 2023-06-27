85781be07096ecfc9ef063ec96c66c60ede81bc6
Pritam Dhara <pritam1813@gmail.com>
2023-06-27 12:42:19 +0530

    Installed Redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
-import './assets/css/index.css'
+import { configureStore } from '@reduxjs/toolkit';
+
+import App from './components/App.jsx';
+import './assets/css/index.css';
+import movies from './reducers';
+
+const store = configureStore({reducer: movies});
+console.log('Store', store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
