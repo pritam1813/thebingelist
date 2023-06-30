c4a902081ab048e456bd89b2142c01e413f45ff9
Pritam Dhara <pritam1813@gmail.com>
2023-06-30 10:48:13 +0530

    Added a middleware funtion with currying method

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
 
-import App from './components/App.jsx';
-import './assets/css/index.css';
-import rootReducer from './reducers';
+import App from "./components/App.jsx";
+import "./assets/css/index.css";
+import rootReducer from "./reducers";
 
-const store = configureStore({reducer: rootReducer});
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       //middleware code
+//       console.log("ACTION_TYPE = ", action.type);
+//       next(action);
+//     };
+//   };
+// };
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//Above function can be written this way too
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    //middleware code
+    console.log("ACTION_TYPE = ", action.type);
+    next(action);
+  };
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [logger],
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App store={store} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
